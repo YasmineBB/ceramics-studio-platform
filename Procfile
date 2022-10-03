b1ee4f9a4fa411e6764b2001476572fbfc46eeca
YasmineBB <y.bourhabi@gmail.com>
2022-10-03 14:44:15 +0000

    Deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ceramics_studio.wsgi
---
+++ studio_platform/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudioPlatformConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'studio_platform'
