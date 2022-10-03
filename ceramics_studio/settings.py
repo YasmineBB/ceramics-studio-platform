fec2936a98ac7cc9ecf761db436dfb6beffb6630
YasmineBB <y.bourhabi@gmail.com>
2022-10-03 14:51:44 +0000

    Update allowed_hosts

--- ceramics_studio/settings.py
+++ ceramics_studio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['studio-ceramics.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['social-ceramics.herokuapp.com', 'localhost']
 
 
 # Application definition
