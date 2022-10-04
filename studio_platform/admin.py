b76bd6df499bfc88e3c7058f8586e40a37029459
YasmineBB <y.bourhabi@gmail.com>
2022-10-04 17:13:52 +0000

    Add CommentAdmin class to allow admin to view, search for and approve comments

--- studio_platform/admin.py
+++ studio_platform/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,6 +7,18 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     search_fields = ['caption', 'description']
-    list_display = ['caption', 'status', 'created_on']
+    list_display = ['caption', 'status', 'created_on', 'user']
+    list_filter = ('status', 'created_on')
     summernote_fields = ('description')
 
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    search_fields = ['user', 'comment_body']
+    list_display = ['user', 'comment_body', 'post', 'posted_on', 'approved']
+    list_filter = ['approved', 'posted_on']
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
