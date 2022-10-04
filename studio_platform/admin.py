2afff3b73a956fbccf92456c2733a4eb0bce1471
YasmineBB <y.bourhabi@gmail.com>
2022-10-04 16:18:33 +0000

    Add search_fields and list_display code to admin class

--- studio_platform/admin.py
+++ studio_platform/admin.py
@@ -6,5 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    search_fields = ['caption', 'description']
+    list_display = ['caption', 'status', 'created_on']
     summernote_fields = ('description')
 
