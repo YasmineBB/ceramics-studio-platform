b0680d9a011b27851a1e8c374e440da6da9e1023
YasmineBB <y.bourhabi@gmail.com>
2022-10-04 16:15:10 +0000

    Add Comment model and rename posted_on field to created_on in Post model

--- studio_platform/models.py
+++ studio_platform/models.py
@@ -16,14 +16,14 @@ class Post(models.Model):
     caption = models.CharField(max_length=200, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='posts')
-    posted_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
     image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=POST_STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
 
     class Meta:
-        ordering = ['-posted_on']
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.caption
@@ -32,3 +32,21 @@ class Post(models.Model):
         return self.likes.count()
 
 
+"""
+Model for commenting on a post.
+"""
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comment')
+    user = models.CharField(max_length=50)
+    comment_body = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-posted_on']
+
+    def __str__(self):
+        return f'Comment {self.comment_body} by {self.user}'
