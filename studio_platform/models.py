e19d5caa3ce2c9eb9c30d828eb8aa8e115489610
YasmineBB <y.bourhabi@gmail.com>
2022-10-25 16:27:38 +0000

    Update unique to false on caption and slug fields for user upload

--- studio_platform/models.py
+++ studio_platform/models.py
@@ -13,8 +13,8 @@ model for uploading a post.
 
 
 class Post(models.Model):
-    caption = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(null=False, unique=True)
+    caption = models.CharField(max_length=200, unique=False)
+    slug = models.SlugField(null=False, unique=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='posts')
     created_on = models.DateTimeField(auto_now_add=True)
